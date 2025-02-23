20ee49b9c77bf3eb11cb47592ca8a68a6197ca0b
Genosss <gunturhanabi222@gmail.com>
2025-02-24 06:09:45 +0700

    Add files via upload

--- finance_manager.sql
+++ finance_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2025 at 08:20 PM
+-- Generation Time: Feb 24, 2025 at 12:08 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,8 +33,16 @@ CREATE TABLE `bills` (
   `amount` decimal(10,2) NOT NULL,
   `due_date` date NOT NULL,
   `is_paid` tinyint(1) DEFAULT 0,
-  `status` varchar(50) DEFAULT NULL
+  `status` varchar(50) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bills`
+--
+
+INSERT INTO `bills` (`id`, `name`, `amount`, `due_date`, `is_paid`, `status`, `user_id`) VALUES
+(2, 'Wifi', 30000.00, '2025-03-20', 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -46,16 +54,9 @@ CREATE TABLE `budgets` (
   `id` int(11) NOT NULL,
   `category` varchar(50) NOT NULL,
   `amount` decimal(10,2) NOT NULL,
-  `month` date NOT NULL
+  `month` date NOT NULL,
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `budgets`
---
-
-INSERT INTO `budgets` (`id`, `category`, `amount`, `month`) VALUES
-(1, 'uang makan', 1500000.00, '0000-00-00'),
-(2, 'uang makan', 300000.00, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -68,15 +69,10 @@ CREATE TABLE `debts` (
   `person` varchar(100) NOT NULL,
   `amount` decimal(10,2) NOT NULL,
   `type` enum('hutang','piutang') NOT NULL,
-  `is_paid` tinyint(1) DEFAULT 0
+  `is_paid` tinyint(1) DEFAULT 0,
+  `due_date` date DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `debts`
---
-
-INSERT INTO `debts` (`id`, `person`, `amount`, `type`, `is_paid`) VALUES
-(1, 'Hanafi', 50000.00, 'hutang', 0);
 
 -- --------------------------------------------------------
 
@@ -90,20 +86,51 @@ CREATE TABLE `transactions` (
   `description` varchar(255) NOT NULL,
   `amount` decimal(10,2) NOT NULL,
   `category` varchar(50) NOT NULL,
-  `type` enum('income','expense') NOT NULL
+  `type` enum('income','expense') NOT NULL,
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `transactions`
 --
 
-INSERT INTO `transactions` (`id`, `date`, `description`, `amount`, `category`, `type`) VALUES
-(1, '2025-02-21', 'Beli Sarden, minyak, minuman', 16000.00, 'uang makan', 'expense'),
-(3, '2025-02-22', 'kebutuhan ', 25000.00, 'jajan', 'expense'),
-(4, '2025-02-21', 'kebutuhan ', 18500.00, 'uang makan', 'expense'),
-(7, '2025-02-22', 'rokok bre', 25000.00, 'jajan', 'expense'),
-(8, '2025-02-22', 'minuman', 6000.00, 'jajan', 'expense'),
-(9, '2025-02-23', 'beli makam', 13000.00, 'jajan', 'expense');
+INSERT INTO `transactions` (`id`, `date`, `description`, `amount`, `category`, `type`, `user_id`) VALUES
+(1, '2025-02-21', 'Ngopi Lek', 16000.00, 'uang makan', 'expense', NULL),
+(3, '2025-02-22', 'kebutuhan ', 25000.00, 'jajan', 'expense', NULL),
+(4, '2025-02-21', 'kebutuhan ', 18500.00, 'uang makan', 'expense', NULL),
+(7, '2025-02-22', 'jajan', 25000.00, 'jajan', 'expense', NULL),
+(8, '2025-02-22', 'minuman', 6000.00, 'jajan', 'expense', NULL),
+(9, '2025-02-23', 'beli makan', 13000.00, 'jajan', 'expense', NULL),
+(10, '2025-02-23', 'beras', 27000.00, 'masak', 'expense', NULL),
+(11, '2025-02-23', 'income', 250000.00, 'transferan', 'income', NULL),
+(13, '2025-02-24', 'jajan', 10000.00, 'jajan', 'expense', NULL),
+(14, '2025-02-24', 'minuman', 1500.00, 'jajan', 'expense', NULL),
+(15, '2025-02-24', 'minuman', 1000.00, 'jajan', 'expense', NULL),
+(16, '2025-02-24', 'Oli Motor', 65000.00, 'Motor', 'expense', NULL),
+(17, '2025-02-24', 'Oli Motor', 65000.00, 'Motor', 'expense', 1),
+(18, '2025-02-24', 'Tf', 250000.00, 'transferan', 'income', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `created_at`) VALUES
+(1, 'guntur', 'gunturhanabi222@gmail.com', 'pbkdf2:sha256:1000000$DQ6VCVGQDvHKgtrM$0b619275b6329457b2e2f421e0049a92ad51778a34f7bfb89ac03325e79adac2', '2025-02-23 15:43:45'),
+(2, 'admin', 'admin@gmail.com', 'pbkdf2:sha256:1000000$Ij3811uEYcRUQFvq$c5834ef9f722d49ad542ca0b669e3526fbe4afd5873fa8074a695d43e7c65c1f', '2025-02-23 22:55:50');
 
 --
 -- Indexes for dumped tables
@@ -134,6 +161,14 @@ ALTER TABLE `transactions`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -141,7 +176,7 @@ ALTER TABLE `transactions`
 -- AUTO_INCREMENT for table `bills`
 --
 ALTER TABLE `bills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `budgets`
@@ -153,13 +188,19 @@ ALTER TABLE `budgets`
 -- AUTO_INCREMENT for table `debts`
 --
 ALTER TABLE `debts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
